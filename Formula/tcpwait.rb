6b2e25c05b4e81e2561ae071d735b5323305993a
Simon Oulevay <simon.oulevay@gmail.com>
2019-04-30 13:24:28 +0200

    Update tcpwait to 2.2.0

--- Formula/tcpwait.rb
+++ Formula/tcpwait.rb
@@ -1,13 +1,13 @@
 class Tcpwait < Formula
   desc 'Wait for TCP endpoints to be reachable'
   homepage 'https://github.com/AlphaHydrae/tcpwait'
-  url 'https://github.com/AlphaHydrae/tcpwait/releases/download/v2.1.0/tcpwait_v2.1.0_darwin_amd64'
-  sha256 '20ee83cea605a64e03d7c96669a4aa5cee08f47668e9949621b0e6ce016ca4c4'
-  version '2.1.0'
+  url 'https://github.com/AlphaHydrae/tcpwait/releases/download/v2.2.0/tcpwait_v2.2.0_darwin_amd64'
+  sha256 'db0372da37337fc510186a72f57450f6f10a87a4588992a5905b97c68d90b465'
+  version '2.2.0'
 
   def install
-    bin.install 'tcpwait_v2.1.0_darwin_amd64'
-    mv bin/'tcpwait_v2.1.0_darwin_amd64', bin/'tcpwait'
+    bin.install 'tcpwait_v2.2.0_darwin_amd64'
+    mv bin/'tcpwait_v2.2.0_darwin_amd64', bin/'tcpwait'
   end
 
   test do
