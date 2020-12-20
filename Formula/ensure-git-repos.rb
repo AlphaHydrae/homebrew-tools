e3dd72bf44e290030423027ec01a74c93e538f95
Simon Oulevay <simon.oulevay@gmail.com>
2020-12-20 11:01:20 +0100

    Update ensure-git-repos to 1.0.1

--- Formula/ensure-git-repos.rb
+++ Formula/ensure-git-repos.rb
@@ -1,8 +1,8 @@
 class EnsureGitRepos < Formula
   desc "Shell script that checks whether a directory only contains Git repositories"
   homepage "https://github.com/AlphaHydrae/ensure-git-repos"
-  url "https://github.com/AlphaHydrae/ensure-git-repos/archive/v1.0.0.tar.gz"
-  sha256 "e50276b0a9216f1e429882e211f60e4fd974957ca8a0c30ac9b08842b72ff6d5"
+  url "https://github.com/AlphaHydrae/ensure-git-repos/archive/v1.0.1.tar.gz"
+  sha256 "17104532dbff9b791996471904733b3bd73160c333995d49a51b4d76df366a99"
 
   def install
     bin.install 'ensure-git-repos.sh'
