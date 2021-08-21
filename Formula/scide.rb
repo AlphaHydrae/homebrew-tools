1f7cc0707c5e8edd99412e03feda6a56c9a7b765
Simon Oulevay <simon.oulevay@gmail.com>
2021-08-21 16:08:41 +0200

    Update scide to 2.1.0

--- Formula/scide.rb
+++ Formula/scide.rb
@@ -6,7 +6,7 @@ class Scide < Formula
   desc "GNU Screen IDE"
   homepage "https://github.com/AlphaHydrae/scide"
   url "https://github.com/AlphaHydrae/scide/archive/v2.1.0.tar.gz"
-  sha256 "9f3b56bf3382c8c5cfab4ac75194542caef89c8ae5382d035c4bb70c47c514f2"
+  sha256 "3df623df78fa21d0baf86a7b86281c3b8f5d57b9b0138b4b0c27693fdbf52289"
 
   def install
     bin.install "bin/scide"
