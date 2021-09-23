b8d66e103bcb92afb95af4e58af750864e774ee9
Simon Oulevay <simon.oulevay@gmail.com>
2021-09-23 20:06:44 +0200

    Update update-asdf-tools to 1.0.0

--- Formula/update-asdf-tools.rb
+++ Formula/update-asdf-tools.rb
@@ -6,7 +6,7 @@ class UpdateAsdfTools < Formula
   desc "Update all asdf tools to the latest version in one command"
   homepage "https://github.com/AlphaHydrae/update-asdf-tools"
   url "https://github.com/AlphaHydrae/update-asdf-tools/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "814e318bf9a0fd1f87fca645e156b01c48751bd1c8a93cc84aeca83e3f9768e7"
 
   def install
     bin.install "bin/update-asdf-tools"
